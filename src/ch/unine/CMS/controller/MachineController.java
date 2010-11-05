251843b6f9f0952d5bc7ddeacff588c3b6761b2d
stephane <stephane@ws102-233.unine.ch>
2010-11-05 12:04:52 +0100

    ty with servlet

--- src/ch/unine/CMS/controller/MachineController.java
+++ src/ch/unine/CMS/controller/MachineController.java
@@ -1,6 +1,8 @@
 package ch.unine.CMS.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,7 +26,9 @@ public class MachineController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		System.out.println("Salut");
+		PrintWriter out = response.getWriter();
+		out.print("Salut");
 	}
 
 	/**
