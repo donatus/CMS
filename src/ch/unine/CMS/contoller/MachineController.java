720665db28a5571b5b5ca214dfaa168e686bca31
stephane <stephane@ws102-233.unine.ch>
2010-11-05 09:24:53 +0100

    Fist test

--- src/ch/unine/CMS/contoller/MachineController.java
+++ src/ch/unine/CMS/contoller/MachineController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class MachineController
+ * bla
  */
 public class MachineController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
